3ad05d23a18c48d8059429a5adffa65efc9bfb4c
Jay Jagpal <jay@strangemother.com>
2018-09-24 23:19:58 +0100

    URL Push states

--- interface/static/js/index.js
+++ interface/static/js/index.js
@@ -1,5 +1,16 @@
 var localCache = {}
 
+window.onpopstate = function(event) {
+    // console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+
+    if(event.state == null) {
+        console.log('No previous')
+        return
+    }
+    app.serve(event.state.path, false)
+};
+
+
 var app = new Vue({
     el: "#main"
     , data: {
@@ -49,7 +60,7 @@ var app = new Vue({
             localCache = {}
         }
 
-        , serve(path) {
+        , serve(path, pushUrl=true) {
             this.startTime = +(new Date)
             let render = (function(){
                 let self = this.self
@@ -65,6 +76,10 @@ var app = new Vue({
                     self.itemCount = d.meta.length
                     self.files = d
                     self.is_file = d.is_file
+
+                    if(pushUrl && path != undefined) {
+                        window.history.pushState({ path }, path, '/' + path)
+                    }
 
                     self.$nextTick(function(){
                         //Vue.set(this, 'items', d.keys)
